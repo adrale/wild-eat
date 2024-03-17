adc62e88d7753e1a01af3af735e3e0a0f130e342
Enzo Michel <enzomichel399@gmail.com>
2024-03-17 15:55:46 +0100

    création de la première card en js

--- createCard.js
+++ createCard.js
@@ -1,4 +1,28 @@
-function createCard(title, range, description) {
+let container = document.getElementById("cards")
+
+export function createCard() {
     const card = document.createElement("section");
     card.classList.add("cards");
+    container.appendChild(card);
+
+    const favButton = document.createElement("button");
+    favButton.classList.add("favButton");
+    card.appendChild(favButton);
+
+    const img = document.createElement("img");
+    img.classList.add("iconFav");
+    img.src = "iconFavRose.svg";
+    favButton.appendChild(img);
+
+    const cardTitle = document.createElement("h4")
+    cardTitle.classList.add("cardTitle");
+    cardTitle.innerText = "McDonald's";
+    container.appendChild(cardTitle);
+
+    const cardDesc = document.createElement("p");
+    cardDesc.classList.add("cardDesc");
+    cardDesc.innerText = "Hamburger, wrap, salade, desserts... Sur place ou à emporter.";
+    container.appendChild(cardDesc);
 }
+
+
