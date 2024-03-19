ac96bda4a2f2cb5a82fdb7ddeb2b796cfd2be1ef
Enzo Michel <enzomichel399@gmail.com>
2024-03-19 12:56:26 +0100

    section finished

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let container = document.querySelector('.allCards');
 let containerBrunch = document.querySelector(".allBrunch")
 let containerBar = document.querySelector(".allBars")
+let containerPop = document.querySelector(".all-cards-pop")
 
 function createCard() {
     const card = document.createElement('article');
@@ -18,7 +19,7 @@ function createCard() {
     card.appendChild(cardDistance);
 
     const cardDesc = document.createElement('p');
-    cardDesc.innerText = "le vrai burger New - Yorkais, ses frites, ses cookies";
+    cardDesc.innerText = "Le vrai burger New - Yorkais, ses frites, ses cookies";
     cardDesc.classList.add('desc');
     card.appendChild(cardDesc);
 
@@ -28,8 +29,27 @@ function createCard() {
     cardMore.innerText = "En savoir plus";
     card.appendChild(cardMore);
 
-    const cardAvis = document.createElement("")
+    const articleAvis = document.createElement("article");
+    articleAvis.classList.add("avis-note-prix");
+    card.appendChild(articleAvis);
+
+    const avis = document.createElement("p");
+    avis.classList.add("avis");
+    avis.innerText = "Avis : 293";
+    articleAvis.appendChild(avis);
+
+    const note = document.createElement("p");
+    note.classList.add("note");
+    note.innerText = "Note : 4,8/5";
+    articleAvis.appendChild(note);
+
+    const prix = document.createElement("p");
+    prix.classList.add("prix");
+    prix.innerText = "Prix : 15,70€";
+    articleAvis.appendChild(prix);
+
 };
+createCard();
 createCard();
 createCard();
 
@@ -39,17 +59,17 @@ function createCardBrunch() {
     containerBrunch.appendChild(card);
 
     const cardTitle = document.createElement('p');
-    cardTitle.innerText = "East Street";
+    cardTitle.innerText = "Au Brunch";
     cardTitle.classList.add('nameFirm');
     card.appendChild(cardTitle);
 
     const cardDistance = document.createElement('p');
-    cardDistance.innerText = "(à 268m)";
+    cardDistance.innerText = "(à 423m)";
     cardDistance.classList.add('distance');
     card.appendChild(cardDistance);
 
     const cardDesc = document.createElement('p');
-    cardDesc.innerText = "le vrai burger New - Yorkais, ses frites, ses cookies";
+    cardDesc.innerText = "Un moment de partage autour d'un bon brunch";
     cardDesc.classList.add('desc');
     card.appendChild(cardDesc);
 
@@ -58,8 +78,28 @@ function createCardBrunch() {
     cardMore.classList.add('knowMore');
     cardMore.innerText = "En savoir plus";
     card.appendChild(cardMore);
+
+    const articleAvis = document.createElement("article");
+    articleAvis.classList.add("avis-note-prix");
+    card.appendChild(articleAvis);
+
+    const avis = document.createElement("p");
+    avis.classList.add("avis");
+    avis.innerText = "Avis : 898";
+    articleAvis.appendChild(avis);
+
+    const note = document.createElement("p");
+    note.classList.add("note");
+    note.innerText = "Note : 4,4/5";
+    articleAvis.appendChild(note);
+
+    const prix = document.createElement("p");
+    prix.classList.add("prix");
+    prix.innerText = "Prix : 12,80€";
+    articleAvis.appendChild(prix);
 }
 
+createCardBrunch();
 createCardBrunch();
 createCardBrunch();
 createCardBrunch();
@@ -70,17 +110,17 @@ function createCardBar() {
     containerBar.appendChild(card);
 
     const cardTitle = document.createElement('p');
-    cardTitle.innerText = "East Street";
+    cardTitle.innerText = "My Beers";
     cardTitle.classList.add('nameFirm');
     card.appendChild(cardTitle);
 
     const cardDistance = document.createElement('p');
-    cardDistance.innerText = "(à 268m)";
+    cardDistance.innerText = "(à 308m)";
     cardDistance.classList.add('distance');
     card.appendChild(cardDistance);
 
     const cardDesc = document.createElement('p');
-    cardDesc.innerText = "le vrai burger New - Yorkais, ses frites, ses cookies";
+    cardDesc.innerText = "Boire un verre seul ou avec des amis";
     cardDesc.classList.add('desc');
     card.appendChild(cardDesc);
 
@@ -89,9 +129,62 @@ function createCardBar() {
     cardMore.classList.add('knowMore');
     cardMore.innerText = "En savoir plus";
     card.appendChild(cardMore);
+
+    const articleAvis = document.createElement("article");
+    articleAvis.classList.add("avis-note-prix");
+    card.appendChild(articleAvis);
+
+    const avis = document.createElement("p");
+    avis.classList.add("avis");
+    avis.innerText = "Avis : 487";
+    articleAvis.appendChild(avis);
+
+    const note = document.createElement("p");
+    note.classList.add("note");
+    note.innerText = "Note : 4,9/5";
+    articleAvis.appendChild(note);
+
+    const prix = document.createElement("p");
+    prix.classList.add("prix");
+    prix.innerText = "Prix : 8,70€";
+    articleAvis.appendChild(prix);
 }
 
 createCardBar();
 createCardBar();
 createCardBar();
+createCardBar();
 
+function createCardPop() {
+    const card = document.createElement('article');
+    card.classList.add('cards-populaires');
+    containerPop.appendChild(card);
+
+    const cardTitle = document.createElement('p');
+    cardTitle.innerText = "BIBIBAP";
+    cardTitle.classList.add('nameFirm');
+    card.appendChild(cardTitle);
+
+    const articleAvis = document.createElement("article");
+    articleAvis.classList.add("avis-note-prix");
+    card.appendChild(articleAvis);
+
+    const avis = document.createElement("p");
+    avis.classList.add("avis");
+    avis.innerText = "Avis : 1007";
+    articleAvis.appendChild(avis);
+
+    const note = document.createElement("p");
+    note.classList.add("note");
+    note.innerText = "Note : 4,8/5";
+    articleAvis.appendChild(note);
+
+    const prix = document.createElement("p");
+    prix.classList.add("prix");
+    prix.innerText = "Prix : 13,70€";
+    articleAvis.appendChild(prix);
+};
+
+createCardPop();
+createCardPop();
+createCardPop();
