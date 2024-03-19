d80ff67a042be85bfa1dcab18a1b86697de18355
Delgorithm <delgorithm@gmail.com>
2024-03-19 09:28:34 +0100

    Feautre : Added the HTML/CSS/JS of the navbar

---
+++ script.js
@@ -0,0 +1,25 @@
+const qualityCard = document.querySelector (".aboutUs-quality");
+const qualityHidden = document.querySelector (".qualityHidden");
+
+qualityCard.addEventListener ("click", function () {
+    qualityCard.classList.toggle ("unhideArticle");
+    qualityHidden.classList.toggle ("unhide");   
+})
+
+const availabilityCard = document.querySelector(".aboutUs-availability");
+const availabilityHidden = document.querySelector(".availabilityHidden");
+
+availabilityCard.addEventListener("click", function () {
+    availabilityCard.classList.toggle ("unhideArticle");
+    availabilityHidden.classList.toggle ("unhide");  
+})
+
+const teamCard = document.querySelector(".aboutUs-team");
+const teamHidden = document.querySelectorAll(".teamHidden");
+
+teamCard.addEventListener("click", function () {
+    teamCard.classList.toggle ("unhideArticle");
+    for(let i=0; i<teamHidden.length; i++) {
+        teamHidden[i].classList.toggle ("unhidePicture");
+    }  
+})
