be5acf4e3b968cb0c153e2415cdbd6911b03444f
Enzo Michel <enzomichel399@gmail.com>
2024-03-19 15:46:05 +0100

    m

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 // ----------------------------------------- NAVBAR ---------------------------------
 
-const qualityCard = document.querySelector (".aboutUs-quality");
-const qualityHidden = document.querySelector (".qualityHidden");
+const qualityCard = document.querySelector(".aboutUs-quality");
+const qualityHidden = document.querySelector(".qualityHidden");
+const iconSearch = document.querySelector(".navMobil-search");
 
-qualityCard.addEventListener ("click", function () {
-    qualityCard.classList.toggle ("unhideArticle");
-    qualityHidden.classList.toggle ("unhide");   
-})
+    qualityCard.addEventListener ("click", function () {
+        qualityCard.classList.toggle("unhideArticle");
+        qualityHidden.classList.toggle("unhide");
+    })
 
 const availabilityCard = document.querySelector(".aboutUs-availability");
 const availabilityHidden = document.querySelector(".availabilityHidden");
 
 availabilityCard.addEventListener("click", function () {
-    availabilityCard.classList.toggle ("unhideArticle");
-    availabilityHidden.classList.toggle ("unhide");  
+    availabilityCard.classList.toggle("unhideArticle");
+    availabilityHidden.classList.toggle("unhide");
 })
 
 const teamCard = document.querySelector(".aboutUs-team");
 const teamHidden = document.querySelectorAll(".teamHidden");
 
 teamCard.addEventListener("click", function () {
-    teamCard.classList.toggle ("unhideArticle");
-    for(let i=0; i<teamHidden.length; i++) {
-        teamHidden[i].classList.toggle ("unhidePicture");
-    }  
+    teamCard.classList.toggle("unhideArticle");
+    for (let i = 0; i < teamHidden.length; i++) {
+        teamHidden[i].classList.toggle("unhidePicture");
+    }
+})
+
+iconSearch.addEventListener ("click", function () {
+
 })
 
