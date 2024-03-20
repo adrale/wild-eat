7d2c81cd693acda848cb5f0d06298e7ec3317852
Enzo Michel <enzomichel399@gmail.com>
2024-03-20 10:33:59 +0100

    images et tableau mis a jour

--- cards.js
+++ cards.js
@@ -1,4 +1,43 @@
 export const restaurants = [
+
+    //commandes populaires//
+    {
+        name: "Sushis",
+        type: "Sushis",
+        desc: "Sushis frais, variés et savoureux pour une expérience gustative unique.",
+        img: "img/card-sushis.jpg",
+        range: "(543m)",
+        avis: "Avis : 788",
+        note: "Note : 4,9/5",
+        prix: "Prix : 11,55€",
+    },
+
+    {
+        name: "Riz crousty",
+        type: "Riz crousty",
+        desc: "Expérience culinaire croustillante, variée et délectable pour tous les convives.",
+        img: "img/card-riz crousty.jpg",
+        range: "(907m)",
+        avis: "Avis : 943",
+        note: "Note : 4,7/5",
+        prix: "Prix : 13,55€",
+    },
+
+    {
+        name: "Kebab",
+        type: "Kebab",
+        desc: "Découvrez une sélection de kebabs variés, frais et savoureux.",
+        img: "img/card-kebab.jpg",
+        range: "(235m)",
+        avis: "Avis : 245",
+        note: "Note : 4,2/5",
+        prix: "Prix : 9,55€",
+    },
+
+    //fin commandes populaires//
+
+    
+
     {
         name: "McDonald's",
         type: "Fast-Food",
